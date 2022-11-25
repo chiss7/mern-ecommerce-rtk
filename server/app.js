3f827070a56b9813780571ddf73b9b0f15c67429
Christopher Trujillo <cristru8@gmail.com>
2022-11-25 06:47:41 -0500

    disabled cors options

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ const corsOptions = {
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(
   fileUpload({
     useTempFiles: true,
